496321cc79caa38a1526220b95ae0cd2fb83eaa9
alokomkar <alokomkar.gudikote@gmail.com>
2022-12-05 12:50:50 +0530

    Code cleanup, rename application

--- app/src/main/java/com/alokomkar/starter/MainActivity.kt
+++ app/src/main/java/com/alokomkar/starter/MainActivity.kt
@@ -7,8 +7,7 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.OnBackPressedCallback
-import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.clickable
@@ -247,36 +246,6 @@ private fun NewsCardView(
     }
 
 
-}
-
-@Composable
-fun BackHandler(enabled: Boolean = true, onBack: () -> Unit) {
-    // Safely update the current `onBack` lambda when a new one is provided
-    val currentOnBack by rememberUpdatedState(onBack)
-    // Remember in Composition a back callback that calls the `onBack` lambda
-    val backCallback = remember {
-        object : OnBackPressedCallback(enabled) {
-            override fun handleOnBackPressed() {
-                currentOnBack()
-            }
-        }
-    }
-    // On every successful composition, update the callback with the `enabled` value
-    SideEffect {
-        backCallback.isEnabled = enabled
-    }
-    val backDispatcher = checkNotNull(LocalOnBackPressedDispatcherOwner.current) {
-        "No OnBackPressedDispatcherOwner was provided via LocalOnBackPressedDispatcherOwner"
-    }.onBackPressedDispatcher
-    val lifecycleOwner = LocalLifecycleOwner.current
-    DisposableEffect(lifecycleOwner, backDispatcher) {
-        // Add callback to the backDispatcher
-        backDispatcher.addCallback(lifecycleOwner, backCallback)
-        // When the effect leaves the Composition, remove the callback
-        onDispose {
-            backCallback.remove()
-        }
-    }
 }
 
 //@Composable
